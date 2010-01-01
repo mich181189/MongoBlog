b20d288e3a18b5d6e1ae73d214e39bb6554770a7
Michael Cullen <michael181189@hotmail.com>
2010-01-01 20:08:18 +0000

    restructured and made use of CGI library cgicc

---
+++ include/Template.hpp
@@ -0,0 +1,15 @@
+///////////////////////////////////////////////////////////////////////
+//   								     //
+//   MongBlog - Superfast simple blogging platform                   //
+//   Template Library                                                //
+//   (c) 2010 Michael Cullen http://cullen-online.com                //
+//   Released under the BSD licence as define in the                 //
+//   accompanying LICENCE file. Please read it :-)                   //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+
+class Template {
+public:
+    Template();
+    ~Template();
+};
